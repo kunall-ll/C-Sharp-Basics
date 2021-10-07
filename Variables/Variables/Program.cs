340d8233b0a8e4054e2fa25d5b5da0fce363b9f9
Kunal Agarwala <kunal.agarwala6@gmail.com>
2021-10-07 01:04:08 +0100

    Add files via upload

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Jordan";
+            int age = 123;
+            Console.WriteLine("Name is " + firstName + ", age is: " + age);
+
+            var firstName2 = "Vinesh";
+            var age2 = 4000;
+            Console.WriteLine("Name is " + firstName2 + ", age is " + age2);
+
+            int age3;
+            age3 = 10;
+            Console.WriteLine("Age is: " + age3);
+
+            int int1 = 10, int2 = 20;
+            Console.WriteLine(int1 + int2);
+        }
+    }
+}
